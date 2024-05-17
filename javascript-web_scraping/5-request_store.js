1bfc7292aedbae6dd2af369094d6aa5d8d328926
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-05-17 17:39:01 -0400

    get content and store it

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const data = body;
+    fs.writeFile(filePath, data, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
